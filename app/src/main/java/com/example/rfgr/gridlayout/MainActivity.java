3dd72e3e469963547d86efe86aba10398623e82d
Rafal Granoszewski <rgranoszewski@gmail.com>
2018-08-14 13:04:02 +0200

    Dodane dodatkowe przyciski testowe oraz zmienina logika aplikacji.

--- app/src/main/java/com/example/rfgr/gridlayout/MainActivity.java
+++ app/src/main/java/com/example/rfgr/gridlayout/MainActivity.java
@@ -3,24 +3,42 @@ package com.example.rfgr.gridlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private String messageText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /**Po kliknięciu przycisku zostanie wywołana i wykonana poniższa metoda. Ma ona za zadanie odebrać wartość wpisaną w polu editText i umieścić ją w innym polu textView*/
-    public void sendButton (View view) {
-        EditText messageView = (EditText) findViewById(R.id.adressName);
-        String messageText = messageView.getText().toString();
 
+    /**
+     * Po kliknięciu przycisku zostanie wywołana i wykonana poniższa metoda. Ma ona za zadanie odebrać wartość wpisaną w polu editText i umieścić ją w innym polu textView
+     */
+    public void sendButton(View view) {
         TextView textVi = (TextView) findViewById(R.id.messageWindow);
         textVi.setText(messageText);
+    }
+
+    public void toggleButton(View view) {
+        boolean on = ((ToggleButton) view).isChecked();
+        if (on) {
+            messageText = "Przełącznik jest włączony";
+
+        } else {
+            EditText messageView = (EditText) findViewById(R.id.adressName);
+            String messageText = messageView.getText().toString();
+
+            TextView textVi = (TextView) findViewById(R.id.messageWindow);
+            textVi.setText(messageText);
+        }
     }
 }
