ffa92d6615f81ba237efdc14729f9a056c1e6ade
Rafal Granoszewski <rgranoszewski@gmail.com>
2018-08-16 21:02:23 +0200

    Dodane nowy widok RadioGroup wraz z przykładową logiką.

--- app/src/main/java/com/example/rfgr/gridlayout/MainActivity.java
+++ app/src/main/java/com/example/rfgr/gridlayout/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import org.w3c.dom.Text;
@@ -39,6 +41,49 @@ public class MainActivity extends AppCompatActivity {
 
             TextView textVi = (TextView) findViewById(R.id.messageWindow);
             textVi.setText(messageText);
+        }
+/**
+ *Poniższy kod sprawdza czy checkBox jest zaznaczony w głównej aktywności, i jeżeli jest zaznaczony to wyświetli krótki komunikat
+ * ale w związku z tym że jest częścią klasy toggleButton to sprawdzenie odbywa się tylko i wyłacznie gdy toggleButton jest właczony*/
+        CheckBox checkBoxName = (CheckBox) findViewById(R.id.checkBox);
+        boolean checkBoxBoolean = checkBoxName.isChecked();
+        if (checkBoxBoolean) {
+            Toast toastMessage = Toast.makeText(this, "CheckBox jest zaznaczony", Toast.LENGTH_SHORT);
+            toastMessage.show();
+        }
+
+    }
+
+    //teraz wywołamy metodę onClick na checkBox'ie
+    public void checkBoxOnClick(View view) {
+        boolean checkBoxChecked = ((CheckBox) view).isChecked(); //najpierw sprawdzamy czy checkBox jest zaznaczony
+
+        switch (view.getId()) { //wykorzystuję metodę swich by np, określić który guzik został kliknięty
+            case R.id.checkBox: // przypadek pierwszy dotyczy checkBox'a o takim właśnie imieniu
+                if (checkBoxChecked) //jeżeli jest zaznaczony to walnie na ekran toast
+                    Toast.makeText(this, "CheckBox wywołał metodę on click", Toast.LENGTH_SHORT).show();
+                else //jeżeli natomiast nie jest zaznaczony to niczym nie walnie i nic się nie stanie
+                    break;
+                //w tym miejscu można dodać inny case dla np: drugiego guzika
+                //warto też nadmienić iż w powyższym kodzie znajdują sie dwie metody sprawdzające status tego samego checkBox'a
+                // pierwsza metoda sprawdza status checkBox'a dopiero gdy toggleButton jest włączony, a druga jest niezależna i to właśnie ona zostanie wykonana niezależnie od wciśniętego toggleButton'a
+
+        }
+    }
+
+    /**
+     * Teraz logika obsługi radiobutton'ów
+     */
+    public void guzikClick(View view) {
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGrupa); //najpierw poszukuję odwłoania do grupy radioGroup
+        int id = radioGroup.getCheckedRadioButtonId(); //sprawdzam który z radioButton'ów jest zaznaczony
+        switch (id) { //wykorzystuję swich by wykonać odpowiednią komendę w zależości od wybranego guzika radioButton'a
+            case R.id.guzik_1_name:
+                Toast.makeText(this, "guzik pierwszy", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.guzik_2_name:
+                Toast.makeText(this, "guzik druki", Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 }
